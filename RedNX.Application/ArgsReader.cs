25572f5b4f9d83cc7aad4f417b53828cc4b985af
João Pedro Caires <joaopdante@tryhosting.com.br>
2021-08-17 16:56:31 -0300

    Create ArgsReader

---
+++ RedNX.Application/ArgsReader.cs
@@ -0,0 +1,63 @@
+﻿using RedNX.System;
+using System.Collections.Generic;
+
+namespace RedNX.Application {
+    public class ArgsReader {
+
+        private readonly Dictionary<string, ArgsValues> _argsDictionary = new();
+        private readonly List<string> _extraArgs = new();
+
+        public readonly string KeyIdentifier = RedEnvironment.OperatingSystem == SystemOS.Windows ? "/" : "-";
+
+        public IReadOnlyDictionary<string, ArgsValues> ArgsDictionary => _argsDictionary;
+        public IReadOnlyList<string> ExtraArgs => _extraArgs;
+
+        public ArgsReader() { }
+
+        public ArgsReader(IReadOnlyList<string> args) {
+            Load(args);
+        }
+
+        public void Load(IReadOnlyList<string> args) {
+            var position = 0;
+            while (position < args.Count) {
+                string current = args[position];
+                if (current.StartsWith(KeyIdentifier)) {
+                    if (position + 1 < args.Count) {
+                        string key = current.Remove(current.Length - 1, 1);
+                        string value = args[position + 1];
+                        if (!value.StartsWith(KeyIdentifier)) {
+                            if (_argsDictionary.ContainsKey(key)) _argsDictionary[key].AddValue(value);
+                            else _argsDictionary.Add(current.Remove(current.Length - 1, 1), new ArgsValues(value));
+                            position++;
+                        } else {
+                            if (_argsDictionary.ContainsKey(key)) _argsDictionary[key].AddValue("1");
+                            else _argsDictionary.Add(current.Remove(current.Length - 1, 1), new ArgsValues("1"));
+                        }
+                    }
+                } else {
+                    _extraArgs.Add(current);
+                }
+                position++;
+            }
+        }
+
+        public void Clear() {
+            _argsDictionary.Clear();
+            _extraArgs.Clear();
+        }
+
+        public ArgsValues? Get(string key) {
+            if (_argsDictionary.TryGetValue(key, out var value)) return value;
+            return null;
+        }
+
+        public ArgsValues GetOrDefault(string key, ArgsValues defaultValue) {
+            return _argsDictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
+
+        public bool TryGet(string key, out ArgsValues value) {
+            return _argsDictionary.TryGetValue(key, out value);
+        }
+    }
+}
---
+++ RedNX.Application/ArgsValues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace RedNX.Application {
+    public readonly struct ArgsValues {
+
+        public readonly List<string> _values;
+
+        public ArgsValues(string value) {
+            _values = new List<string> { value };
+        }
+
+        internal void AddValue(string value) {
+            _values.Add(value);
+        }
+
+        public string this[int index] => _values[index];
+
+        public int Count => _values.Count;
+
+        public int IndexOf(string value) => _values.IndexOf(value);
+
+        public bool Contains(string value) => _values.Contains(value);
+
+        public string[] ToArray() => _values.ToArray();
+
+        public override string ToString() {
+            return _values.Count switch {
+                0 => null,
+                1 => _values[0],
+                _ => string.Join(",", _values)
+            };
+        }
+    }
+}
